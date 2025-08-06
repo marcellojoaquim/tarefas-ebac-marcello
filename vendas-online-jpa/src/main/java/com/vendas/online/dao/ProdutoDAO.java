1b38a9427f6c50abe5c84ffe63ffe270149c6fe8
Marcello Joaquim <marcellojoaquim1@hotmail.com>
2025-08-05 22:29:06 -0300

    migracao jdbc para jpa

---
+++ vendas-online-jpa/src/main/java/com/vendas/online/dao/ProdutoDAO.java
@@ -0,0 +1,99 @@
+package com.vendas.online.dao;
+
+import com.vendas.online.dao.generic.GenericDAO;
+import com.vendas.online.domain.Estoque;
+import com.vendas.online.domain.Produto;
+import com.vendas.online.exceptions.DAOException;
+import com.vendas.online.exceptions.TipoChaveNaoEncontradaException;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class ProdutoDAO extends GenericDAO<Produto, String> implements IProdutoDAO {
+
+    public ProdutoDAO() {
+        super();
+    }
+
+    @Override
+    public Class<Produto> getTipoClasse() {
+        return Produto.class;
+    }
+
+    @Override
+    public Boolean cadastrar(Produto entity) throws TipoChaveNaoEncontradaException, DAOException {
+        Boolean result = super.cadastrar(entity);
+
+        EstoqueDAO dao = new EstoqueDAO();
+        Estoque estoque = new Estoque();
+        estoque.setCodigoProduto(entity);
+        estoque.setQuantidade(128);
+        dao.cadastrar(estoque);
+        return result;
+    }
+
+    @Override
+    public void atualizarDados(Produto entity, Produto entityCadastrado) {
+        entityCadastrado.setCodigo(entity.getCodigo());
+        entityCadastrado.setDescricao(entity.getDescricao());
+        entityCadastrado.setNome(entity.getNome());
+        entityCadastrado.setValor(entity.getValor());
+        entityCadastrado.setFabricante(entity.getFabricante());
+    }
+
+    @Override
+    protected String getQueryInsercao() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("INSERT INTO TB_PRODUTO ");
+        sb.append("(ID, CODIGO, NOME, DESCRICAO, VALOR, FABRICANTE)");
+        sb.append("VALUES (nextval('sq_produto'),?,?,?,?,?)");
+        return sb.toString();
+    }
+
+    @Override
+    protected String getQueryExclusao() {
+        return "DELETE FROM TB_PRODUTO WHERE CODIGO = ?";
+    }
+
+    @Override
+    protected String getQueryAtualizacao() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("UPDATE TB_PRODUTO ");
+        sb.append("SET CODIGO = ?,");
+        sb.append("NOME = ?,");
+        sb.append("DESCRICAO = ?,");
+        sb.append("VALOR = ?,");
+        sb.append("FABRICANTE = ?");
+        sb.append(" WHERE CODIGO = ?");
+        return sb.toString();
+    }
+
+    @Override
+    protected void setParametrosQueryInsercao(PreparedStatement stmInsert, Produto entity) throws SQLException {
+        stmInsert.setString(1, entity.getCodigo());
+        stmInsert.setString(2, entity.getNome());
+        stmInsert.setString(3, entity.getDescricao());
+        stmInsert.setBigDecimal(4, entity.getValor());
+        stmInsert.setString(5, entity.getFabricante());
+    }
+
+    @Override
+    protected void setParametrosQueryExclusao(PreparedStatement stmDelete, String valor) throws SQLException {
+        stmDelete.setString(1, valor);
+    }
+
+    @Override
+    protected void setParametrosQueryAtualizacao(PreparedStatement stmUpdate, Produto entity) throws SQLException {
+        stmUpdate.setString(1, entity.getCodigo());
+        stmUpdate.setString(2, entity.getNome());
+        stmUpdate.setString(3, entity.getDescricao());
+        stmUpdate.setBigDecimal(4, entity.getValor());
+        stmUpdate.setString(5, entity.getFabricante());
+        stmUpdate.setString(6, entity.getCodigo());
+    }
+
+    @Override
+    protected void setParametrosQuerySelect(PreparedStatement stmSelect, String valor) throws SQLException {
+        stmSelect.setString(1, valor);
+    }
+}
---
+++ vendas-online-jpa/src/main/java/com/vendas/online/domain/Cliente.java
@@ -0,0 +1,98 @@
+package com.vendas.online.domain;
+
+import com.vendas.online.dao.Persistente;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "TB_CLIENTE")
+public class Cliente implements Persistente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "cliente_seq")
+    @SequenceGenerator(name = "cliente_seq", sequenceName = "seq_cliente", initialValue = 1)
+    private Long id;
+
+    @Column(name = "nome", nullable = false, length = 65)
+    private String nome;
+
+    @Column(name = "sobreNome", nullable = false, length = 65)
+    private String sobreNome;
+
+    @Column(name = "cpf", nullable = false, unique = true)
+    private Long cpf;
+
+    @Column(name = "tele", nullable = false)
+    private Long tel;
+
+    @Column(name = "end", nullable = false, length = 100)
+    private String end;
+
+    @Column(name = "numero", nullable = false)
+    private Integer numero;
+
+    @Column(name = "cidade", nullable = false, length = 50)
+    private String cidade;
+
+    @Column(name = "estado", nullable = false, length = 50)
+    private String estado;
+
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSobreNome() {
+        return sobreNome;
+    }
+
+    public void setSobreNome(String sobreNome) {
+        this.sobreNome = sobreNome;
+    }
+
+    public Long getCpf() {
+        return cpf;
+    }
+    public void setCpf(Long cpf) {
+        this.cpf = cpf;
+    }
+    public Long getTel() {
+        return tel;
+    }
+    public void setTel(Long tel) {
+        this.tel = tel;
+    }
+    public String getEnd() {
+        return end;
+    }
+    public void setEnd(String end) {
+        this.end = end;
+    }
+    public Integer getNumero() {
+        return numero;
+    }
+    public void setNumero(Integer numero) {
+        this.numero = numero;
+    }
+    public String getCidade() {
+        return cidade;
+    }
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+    public String getEstado() {
+        return estado;
+    }
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+    public Long getId() {
+        return id;
+    }
+
+    @Override
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
